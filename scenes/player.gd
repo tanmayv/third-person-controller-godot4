4b7539f20e9f6b13d6f87210bf7238ec4efa02aa
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2024-01-06 22:33:31 +0530

    Working Controller

---
+++ scenes/player.gd
@@ -0,0 +1,58 @@
+extends CharacterBody3D
+
+@onready var camera_mount = $camera_mount
+@onready var animation_player = $visuals/mixamo_base/AnimationPlayer
+@onready var visuals = $visuals
+
+var SPEED = 3
+var movement_animation = "walking"
+const JUMP_VELOCITY = 4.5
+@export var sens_horizontal = 0.2
+@export var sens_vertical = 0.2
+@export var walking_speed = 3.0
+@export var running_speed = 5.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
+		visuals.rotate_y(deg_to_rad(event.relative.x * sens_horizontal))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y * sens_vertical))
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("run"):
+		SPEED = running_speed
+		movement_animation = "running"
+	if Input.is_action_just_released("run"):
+		SPEED = walking_speed
+		movement_animation = "walking"
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		if animation_player.current_animation != movement_animation:
+			animation_player.play(movement_animation)
+		visuals.look_at(position + direction)
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		if animation_player.current_animation != "idle":
+			animation_player.play("idle")
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
